b3db598f03c8ad52ffe6bea3bd3de2f9b36ec79d
Aisha\ZDC-AISHA <sivakavi63@gmail.com>
2018-04-09 15:10:49 +0530

    dashboard link

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -7,35 +7,43 @@
     <div style="margin-top:80px;">
         <div class="row">
             <div class="animated flipInY col-lg-6 col-md-6 col-sm-12 col-xs-12">
-                <div class="tile-stats" style="padding:20px 0px !important;">
-                    <div class="icon" style="top:45px !important;right:80px !important;"><i class="fa fa-institution" style="font-size:80px !important;"></i></div>
-                    <div class="count">{{$departments}}</div>
-                    <h3>Departments</h3>
-                </div>
+                <a href="{{ route('admin.departments.index') }}">
+                    <div class="tile-stats" style="padding:20px 0px !important;">
+                        <div class="icon" style="top:45px !important;right:80px !important;"><i class="fa fa-institution" style="font-size:80px !important;"></i></div>
+                        <div class="count">{{$departments}}</div>
+                        <h3>Departments</h3>
+                    </div>
+                </a>
             </div>
             <div class="animated flipInY col-lg-6 col-md-6 col-sm-12 col-xs-12">
-                <div class="tile-stats" style="padding:20px 0px !important;">
-                    <div class="icon" style="top:45px !important;right:80px !important;"><i class="fa fa-cubes" style="font-size:80px !important;"></i></div>
-                    <div class="count">{{$shifts}}</div>
-                    <h3>Shifts</h3>
-                </div>
+                <a href="{{ route('admin.shifts.index') }}">
+                    <div class="tile-stats" style="padding:20px 0px !important;">
+                        <div class="icon" style="top:45px !important;right:80px !important;"><i class="fa fa-cubes" style="font-size:80px !important;"></i></div>
+                        <div class="count">{{$shifts}}</div>
+                        <h3>Shifts</h3>
+                    </div>
+                </a>
             </div>
         </div>
         <br/>
         <div class="row">
             <div class="animated flipInY col-lg-6 col-md-6 col-sm-12 col-xs-12">
-                <div class="tile-stats" style="padding:20px 0px !important;">
-                    <div class="icon" style="top:45px !important;right:80px !important;"><i class="fa fa-user" style="font-size:80px !important;"></i></div>
-                    <div class="count">{{$users}}</div>
-                    <h3>Users</h3>
-                </div>
+                <a href="{{ route('admin.users') }}">
+                    <div class="tile-stats" style="padding:20px 0px !important;">
+                        <div class="icon" style="top:45px !important;right:80px !important;"><i class="fa fa-user" style="font-size:80px !important;"></i></div>
+                        <div class="count">{{$users}}</div>
+                        <h3>Users</h3>
+                    </div>
+                </a>
             </div>
             <div class="animated flipInY col-lg-6 col-md-6 col-sm-12 col-xs-12">
-                <div class="tile-stats" style="padding:20px 0px !important;">
-                    <div class="icon" style="top:45px !important;right:80px !important;"><i class="fa fa-child" style="font-size:80px !important;"></i></div>
-                    <div class="count">{{$employees}}</div>
-                    <h3>Employees</h3>
-                </div>
+                <a href="{{ route('admin.employees.index') }}">
+                    <div class="tile-stats" style="padding:20px 0px !important;">
+                        <div class="icon" style="top:45px !important;right:80px !important;"><i class="fa fa-child" style="font-size:80px !important;"></i></div>
+                        <div class="count">{{$employees}}</div>
+                        <h3>Employees</h3>
+                    </div>
+                </a>
             </div>
         </div>
     </div>
